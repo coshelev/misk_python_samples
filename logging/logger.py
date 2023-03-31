ff04505511d30d63e38e9f1a95fc83dddf1e06ab
coshelev <coshelev@gmail.com>
2023-03-31 22:02:59 +0300

    logger modified

--- logging/logger.py
+++ logging/logger.py
@@ -10,8 +10,13 @@ _log_format = f'%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(func
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+
     file_handler = logging.FileHandler("./log.log")
     file_handler.setLevel(logging.INFO)
+
+    #_log_format = f'%(asctime)s - [%(levelname)s] - %(funcName)s(%(lineno)d) - %(message)s'
+    _log_format = f'%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
     file_handler.setFormatter(logging.Formatter(_log_format))
+
     logger.addHandler(file_handler)
     return logger
